0b45bd99b8064f681fa7337998178e68ab5f98ca
yuzuniizawa <yuzu.ringo0504@gmail.com>
2024-10-11 18:10:36 +0900

    add 回答を記述

--- test_app/functions.php
+++ test_app/functions.php
@@ -1,11 +1,85 @@
-<?php
+<?php //データの受け取り・受け渡しとDBへの処理を依頼する機能をまとめる（関数がある）ファイル
 require_once('connection.php');
-function createData($post)
+session_start(); 
+
+// SESSIONにtokenを格納する
+function setToken()
+{
+    $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(16));//ランダム16文字のバイト文字列を作り2進数から16進数に変換した値を$_SESSION['token']に代入
+} //bin2hex：stringを16進数に変換した文字列を返す→
+
+// SESSIONに格納されたtokenのチェックを行い、SESSIONにエラー文を格納する
+function checkToken($token)
+{
+    if (empty($_SESSION['token']) || ($_SESSION['token'] !== $token)) { //
+        $_SESSION['err'] = '不正な操作です';
+        redirectToPostedPage();
+    }
+}
+
+function unsetError()
+{
+    $_SESSION['err'] = '';
+}
+
+function redirectToPostedPage()
+{
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    exit();
+}
+
+function e($text)//エスケープ処理
+{
+    return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
+}
+
+function createData($post)//関数の中に関数がある
 {
   createTodoData($post['content']); // ここを追記
 }
 
-function getTodoList() // connection.php に記述した関数を呼び出す関数を実装する
+function getTodoList() // connection.php に記述した関数を呼び出す関数を実装する データの一覧を表示を行う
 {
     return getAllRecords();//$dbh->query($sql)->fetchAll();が戻り値
+}
+
+function getSelectedTodo($id)//getTodoTextById($id)関数を実行している
+{
+    return getTodoTextById($id); 
+}
+
+function savePostedData($post)//getRefererPath()関数で定義したリクエスト元のページのパスで条件分岐し処理を振り分けている
+{   
+    checkToken($post['token']);//サーバー側とクライアント側のトークンの整合性を行う
+    validate($post);
+    $path = getRefererPath();
+    switch ($path) {
+        case '/new.php': //新規作成のページからPOSTされたなら
+            createTodoData($post['content']);
+            break;
+        case '/edit.php': //編集ページからPOSTされたなら
+            updateTodoData($post);//connection.phpでデータを更新する処理をしている
+            break;
+        case '/index.php': //削除処理のリクエスト元（index.php）からPOSTされたなら
+            deleteTodoData($post['id']);
+            break;
+        default:
+            break;
+        }
+}
+
+function validate($post)//入力された値が、制限通りの内容になっているかを検証
+{
+    if (isset($post['content']) && $post['content'] === '') {
+        $_SESSION['err'] = '入力がありません';
+        redirectToPostedPage();
+    }
+}//送信されてきた情報が空の場合「入力がありません」と表示させる
+
+function getRefererPath()//リクエスト元のURLを文字列で取得しそのパスを返す関数を定義
+{
+    $urlArray = parse_url($_SERVER['HTTP_REFERER']);
+    // var_dump($urlArray);
+    // exit();
+    return $urlArray['path'];
 }
--- test_app/index.php
+++ test_app/index.php
@@ -1,5 +1,7 @@
 <?php //追記 HTMLとPHPを混合して書く方法
-require_once('functions.php'); //追記
+require_once('functions.php'); 
+header('Set-Cookie: userId=123');
+setToken(); //functions.phpで定義したsetToken関数を呼び出している。ここではstore.phpへのリクエストは  new.php, edit.php, index.php からのみに限定している
 ?> 
 
 <!DOCTYPE html>
@@ -9,7 +11,9 @@ require_once('functions.php'); //追記
   <title>Home</title>
 </head>
 <body>
-  welcome hello world
+  <?php if (!empty($_SESSION['err'])): ?>
+    <p><?= $_SESSION['err']; ?></p> 
+  <?php endif; ?>
   <div>
      <a href="new.php">
        <p>新規作成</p>
@@ -26,14 +30,16 @@ require_once('functions.php'); //追記
       <?php foreach (getTodoList() as $todo): ?> 
         <!-- =はechoの省略形。HTMLとphpが混在してるときphpタグ内でechoを使用すこの書き方  -->
         <tr>
-          <td><?= $todo['id']; ?></td>
-          <td><?= $todo['content']; ?></td>
+        <td><?= e($todo['id']); ?></td> 
+        <td><?= e($todo['content']); ?></td> 
           <td>
-            <a href="">更新</a>
+          <a href="edit.php?id=<?= e($todo['id']); ?>">更新</a>
+           <!-- ?1以下はクエリパラメータ（サーバーへ送りたいデータを指定するためにURLの最後に追加する文字列）：edit.phpに遷移し、クエリパラメータのデータをGET（GETはなにか情報を検索したり取得するためのメソッド）でedit.phpに送ることができる。 -->
           </td>
           <td>
             <form action="store.php" method="post">
-              <input type="hidden" name="id" value="">
+              <input type="hidden" name="id" value="<?= e($todo['id']); ?>"> <!-- 編集 -->
+              <input type="hidden" name="token" value="<?= $_SESSION['token']; ?>">
               <button type="submit">削除</button>
             </form>
           </td>
@@ -41,5 +47,7 @@ require_once('functions.php'); //追記
       <?php endforeach; ?> <!-- HTTMLと混在してPHPの処理を書くときにのみ、：とendforach;とかく-->
     </table>
   </div>
+  <?php unsetError(); ?>
+  <!-- sessionのキーであるerrに格納したエラーメッセージを空文字にして、ブラウザ上にエラーメッセージを表示させないようにする-->
 </body>
 </html>
